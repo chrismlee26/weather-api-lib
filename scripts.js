d76eee8b61c84ce51aed439e7b935c4825ea9902
CHRIS_LEE <chrismlee87@gmail.com>
2021-02-15 19:32:14 -0800

    challenge 3 complete refactored and cleaned

--- scripts.js
+++ scripts.js
@@ -1,22 +1,13 @@
 // Functions 
 
-async function getWeather(zip, apiKey, weatherCallBack, units='imperial') {
+function getWeather(zip, apiKey, units='imperial') {
   // const units = 'imperial'
   const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-  fetch(path) // fetch the path
-    .then((res) => { // promise to return a response (named res)
-      return res.json() // return the promise-- response (res) as json
-    }).then(json => { // stream json through callback
-      weatherCallBack(json) // json streams into this object
-    })
-    .catch( failCallBack ) //handle errors
-    
-    function failCallBack() {
-      (err => console.log(err.message))
-    }
+  
+  const resPromise = fetch(path) //variable to fetch with path
+  const jsonPromise = resPromise.then(res => res.json()) //fetch resolves promise and returns a result.json()
+  
+  return jsonPromise
 }
-
-
-
 
 export { getWeather };
